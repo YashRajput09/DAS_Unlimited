558a103c53879841acd877d150c5e356ab4512ad
Yash Rajput <rajputyash8561@gmail.com>
2025-09-14 01:54:57 +0530

    solve 2d array problems

---
+++ arrays/2D_Arrays/SearchInSortedMatrix.java
@@ -0,0 +1,26 @@
+public class SearchInSortedMatrix {
+    public static void SISM(int matrix[][], int key) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == key) {
+                System.out.print("Key " + matrix[row][col] + " found at index: (" + row + "," + col + ")");
+                return;
+            } else if (key < matrix[row][col]) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        System.out.println("Invalid key: " + key);
+    }
+
+    public static void main(String[] args) {
+        int matrix[][] = { { 10, 20, 30, 40 },
+                { 15, 25, 35, 45 },
+                { 27, 29, 37, 48 },
+                { 32, 33, 39, 50 } };
+        SISM(matrix, 33);
+    }
+}
